cf259787096190e31484a153b4c6a2e0fda0abe4
Diva Lonial <diva.lonial@gmail.com>
2021-05-29 16:41:09 +0800

    basic query

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql')
+const app = express()
+
+const schema = new GraphQLSchema({
+
+    // query with : query { message }
+    query: new GraphQLObjectType({
+        name: "HelloWorld",
+        fields: () => ({
+            message: {
+                type: GraphQLString,
+                resolve: () => "Hello World"
+            }
+        })
+    })
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}))
+app.listen(5000, () => console.log("Server started"))
